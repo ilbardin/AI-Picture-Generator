0ebf06aee17a560356ffe0cc9c6dbdd4905e7abc
Ignacio Bardin <ilbardin@gmail.com>
2023-02-22 11:44:10 -0300

    Initial setup and commit.

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App(props) {
+    return (
+        <h1 className={"text-3x1 font-bold underline"}>
+            Hello World!
+        </h1>
+    );
+}
+
+export default App;
